6f518046f60b355003716e4c0c7f772348a4f2ea
shreyasrami <shreyasrami24.sr@gmail.com>
2020-07-17 15:11:38 +0530

    Added url changes for authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,6 @@ urlpatterns=[
    
     path("register/",views.register,name="register"),
     path("login",views.login,name="login"),
-    path("logout",views.logout,name="logout"),
-    path('register/login',views.login,name='login'),
-    path('register/register',views.register,name='register')
-    
+    path("logout",views.logout,name="logout")
 
 ]
